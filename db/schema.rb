0896a5f446e1b0e041c7c83c9297424fea9bbd52
mikasa-june <59766405+mikasa-june@users.noreply.github.com>
2020-02-14 10:38:53 +0900

    発送元の地域の選択機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_112531) do
+ActiveRecord::Schema.define(version: 2020_02_14_013305) do
+
+  create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
