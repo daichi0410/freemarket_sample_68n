0b668ba13057880de13754232ff093d208f217e2
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-17 16:14:24 +0900

    トップページエラーの調整

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "items#show"
+  root to: "items#index"
 
 end
