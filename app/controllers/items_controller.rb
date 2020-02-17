cc1844305b50c1a66a77c5c8a0a77d89d9c131fc
YUCKHAM <yunara.922@gmail.com>
2020-02-17 20:55:52 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,16 @@ class ItemsController < ApplicationController
     @sales = Item.where(sold_out: 0)
     @sold_outs= Item.where(sold_out: 1)
   end
-
-  def show
-    
-  end
   # @sales発売中のitemを配列に代入
   # @sold_outs売り切れのitemを配列に代入
   
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to("/")
+  end
 end
