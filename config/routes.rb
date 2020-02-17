20531394be274b0ba0740bf65ccaa13e1dc8d17f
YUCKHAM <yunara.922@gmail.com>
2020-02-17 12:42:46 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "test#index"
+  root to: "items#show"
+
 end
