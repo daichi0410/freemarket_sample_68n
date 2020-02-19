b3e9cedefcd062875796239f6cf5516452346827
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-19 11:55:39 +0900

    部分テンプレートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "mypage#index"
+  root to: "items#index"
   resources :mypage, only: [:index, :destroy]
   resources :items, only: [:index, :show, :destroy]
 
