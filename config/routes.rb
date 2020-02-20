391ec70211ff5d8881d62b4dac1b8818a9a13758
Shohei Kuroda <shokuro705@gmail.com>
2020-02-20 18:03:57 +0900

    delete_comment

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root to: "items#new"
   resources :mypage, only: [:index, :destroy]
