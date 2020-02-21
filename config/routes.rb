3b480bd7f0a79daa54eaee5b460ca956d828fc23
Shohei Kuroda <shokuro705@gmail.com>
2020-02-21 17:30:52 +0900

    add_PAYJP_registration_delete_purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'purchase/index'
+  get 'purchase/done'
   get 'card/new'
   get 'card/show'
   devise_for :users, controllers: {
@@ -21,6 +23,14 @@ root to: "items#index"
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
+    end
+  end
+
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
     end
   end
   
