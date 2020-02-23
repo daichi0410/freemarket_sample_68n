3be0ecf6ba9cead57f7a45326ebe962105f5e628
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-23 16:00:49 +0900

    マイページをjqueryの記述に変更した。

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,31 +1,20 @@
 window.addEventListener("turbolinks:load", function() {
   // タブのDOM要素を取得し、変数で定義する。
-  let tabs = document.getElementsByClassName("hako__right__1__middle__left");
-  let btns = document.getElementsByClassName("hako__right__2__middle__left");
-  tabsAry = Array.prototype.slice.call(tabs);
-  btnsAry = Array.prototype.slice.call(btns);
-  // クラスの切り替えをtabswitch関数で定義
+  let tabs = $(".hako__right__1__middle__left");
+  let btns = $(".hako__right__2__middle__left");
   function tabSwitch() {
-    document.getElementsByClassName("active")[0].classList.remove("active");
-    this.classList.add("active");
-    document.getElementsByClassName("show")[1].classList.remove("show");
-    const index = tabsAry.indexOf(this);
-    document.getElementsByClassName("hako__right__1__under")[index].classList.add("show");
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = tabs.index(this);
+    $(".hako__right__1__under").removeClass("show").eq(index).addClass("show");
   }
   function btnSwitch() {
-    document.getElementsByClassName("move")[0].classList.remove("move");
-    this.classList.add("move");
-    document.getElementsByClassName("see")[0].classList.remove("see");
-    const index = btnsAry.indexOf(this);
-    document.getElementsByClassName("hako__right__2__under")[index].classList.add("see");
+    $(".move").removeClass("move");
+    $(this).addClass("move");
+    const index = btns.index(this);
+    $(".hako__right__2__under").removeClass("see").eq(index).addClass("see");
   }
-  tabsAry.forEach(function(value) {
-    // ①`value.`の後に、イベントリスナーでクリックイベントが発生した時に、tabSwitch関数を呼び出す処理を書きましょう。
-    value.addEventListener("click", tabSwitch);
-  });
-  btnsAry.forEach(function(value) {
-    // ①`value.`の後に、イベントリスナーでクリックイベントが発生した時に、tabSwitch関数を呼び出す処理を書きましょう。
-    value.addEventListener("click", btnSwitch);
-  });
-});
 
+  tabs.click(tabSwitch);
+  btns.click(btnSwitch);
+});
