e4a894d7778a57d0bebc83bf1d53bff6f3710220
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-24 10:37:41 +0900

    自動デプロイのため、いったんコミット

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -1,10 +1,10 @@
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
 
- var boxes = $(".nav__left__category__inner");
+//  var boxes = $(".nav__left__category__inner");
 
- function boxAdd(){
-  $(this).addClass("active");
-  $(".nav__left__category__inner__box").addClass("appear");
- }
- boxes.onmouce(boxAdd);
-});
+//  function boxAdd(){
+//   $(this).addClass("active");
+//   $(".nav__left__category__inner__box").addClass("appear");
+//  }
+//  boxes.onmouce(boxAdd);
+// });
