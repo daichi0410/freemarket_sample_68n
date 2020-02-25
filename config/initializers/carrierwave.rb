c336d6190b29552600c4e09fe4f872cee7bccea2
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-25 15:24:31 +0900

    debug deploy2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,15 +5,15 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境:AWS使用
     config.storage = :file
-    # config.fog_provider = 'fog/aws'
-    # config.fog_credentials = {
-    #   provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
-    #   aws_access_key_id: Rails.application.credentials.aws[:access_key_id], #credentails.ymlに鍵の本体があります
-    #   aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  #credentails.ymlに鍵の本体があります
-    #   region:'ap-northeast-1' #'AWSで設定した地域（おそらく'ap-northeast-1') '
-    #   }
-    # config.fog_directory  = 'mercary5teamn'
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercary5teamn'
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      aws_access_key_id: 'Rails.application.credentials.aws[:access_key_id]', #credentails.ymlに鍵の本体があります
+      aws_secret_access_key: 'Rails.application.credentials.aws[:secret_access_key]',  #credentails.ymlに鍵の本体があります
+      region:'ap-northeast-1' #'AWSで設定した地域（おそらく'ap-northeast-1') '
+      }
+    config.fog_directory  = 'mercary5teamn'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercary5teamn'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? 
