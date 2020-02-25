6368fba9598139212474cd2e63b7ab652b36cc6c
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-02-25 20:35:39 +0900

    商品購入画面の修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,13 +2,13 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
       redirect_to controller: "card", action: "new"
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
